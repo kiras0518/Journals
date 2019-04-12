ea170103c146b061beaf50cb89aff6e2d18daf92
雨夜 <kiras0518@gmail.com>
2019-04-12 17:58:39 +0800

    主畫面

---
+++ Journalss/CoreTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  CoreTableViewController.swift
+//  Journalss
+//
+//  Created by YU on 2019/4/12.
+//  Copyright © 2019 ameyo. All rights reserved.
+//
+
+import UIKit
+
+class CoreTableViewController: UITableViewController {
+    
+    @IBOutlet var noteTableView: UITableView!
+    var asdfsadf = ["1","2","3"]
+    var noteObject = [NoteModel]()
+    var selectedIndexPath: IndexPath?
+    
+    @IBAction func buttonCreateNote(_ sender: UIBarButtonItem) {
+        self.performSegue(withIdentifier: "toCreate", sender: Any?.self)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toShow" {
+            if let createVC = segue.destination as? CreateNoteViewController {
+
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return asdfsadf.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "core_cell", for: indexPath) as? CoreTableViewCell {
+            cell.notesTitle.text = asdfsadf[indexPath.row]
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
