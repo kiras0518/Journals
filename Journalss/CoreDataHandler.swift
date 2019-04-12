85baeeee013ef1c779b5d512594ee6cac25db9a4
雨夜 <kiras0518@gmail.com>
2019-04-12 17:56:47 +0800

    init CoreData

---
+++ Journalss/CoreDataHandler.swift
@@ -0,0 +1,48 @@
+//
+//  CoreData.swift
+//  Journalss
+//
+//  Created by YU on 2019/4/12.
+//  Copyright © 2019 ameyo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataHandler {
+    
+    private func getContext() -> NSManagedObjectContext {
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        return appdelegate.persistentContainer.viewContext
+    }
+    
+    func createNotes(helper: NoteModel) {
+        
+        let context = getContext()
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Notes", in: context)
+        let managedObject = NSManagedObject(entity: entity!, insertInto: context)
+        
+        managedObject.setValue(helper.title, forKey: "title")
+        managedObject.setValue(helper.content, forKey: "content")
+        managedObject.setValue(helper.image, forKey: "imageData")
+        
+        do {
+            try context.save()
+        } catch  {
+            print("Error creating notes")
+        }
+    }
+    
+    func getNotesList() -> [Notes] {
+        let context = getContext()
+        var notes : [Notes]? = nil
+        do {
+            notes = try context.fetch(Notes.fetchRequest())
+        } catch {
+            print(error)
+        }
+        return notes!
+    }
+    
+}
